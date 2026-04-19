7963251ff6141eb68c53c627b763b45ce8f58cf0
Ednardo Gomes <ednardogomesdev@gmail.com>
2026-04-19 18:28:34 -0300

    feat: add ModuleCompanyAccess and HelpdeskModule to CompaniesModule imports

--- backend/src/iam/companies/companies.module.ts
+++ backend/src/iam/companies/companies.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './entities/company.entity';
 import { User } from '../users/entities/user.entity';
+import { ModuleCompanyAccess } from '../../helpdesk-modules/entities/module-company-access.entity';
+import { HelpdeskModule } from '../../helpdesk-modules/entities/helpdesk-module.entity';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company, User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Company,
+      User,
+      ModuleCompanyAccess,
+      HelpdeskModule,
+    ]),
+  ],
   controllers: [CompaniesController],
   providers: [CompaniesService],
   exports: [CompaniesService],
